7b2ed722b39256dfe89867a346f7db1b2a9ac6fd
Remus-git <rem36703@gmail.com>
2021-09-08 21:03:37 +0630

    add drawer menu

---
+++ scrpt.js
@@ -0,0 +1,6 @@
+let hamburgerBtn  = document.querySelector(".fa-bars")
+let slideMenu  = document.querySelector(".slideMenu")
+let bars = document.querySelector(".fas fa-bars")
+hamburgerBtn.onclick=function(){
+    slideMenu.classList.toggle("active")
+}
